ccbd55de5db7aa0039d4ea0a070d05b73f5b6ed5
Saeed Heydari <saeedhpro@gmail.com>
2024-05-02 10:33:26 +0330

    feature: add bottom navigation drawer

--- components/footer/BottomNavigationBox.vue
+++ components/footer/BottomNavigationBox.vue
@@ -2,29 +2,28 @@
   <div class="flex flex-col justify-center text-xs font-medium text-right w-full fixed bottom-0 left-0 right-0">
     <nav class="flex justify-between px-4 bg-pink-600 rounded-tl-3xl rounded-tr-3xl">
       <div class="flex justify-between text-white w-full">
-        <div class="flex flex-col items-center">
-          <div class="shrink-0 bg-yellow-300 rounded-2xl h-[3px] w-[45px]"></div>
-          <div class="flex overflow-hidden relative flex-col px-2 py-2 aspect-[1.36] fill-[linear-gradient(180deg,)] w-[72px]">
-            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/59dcc2eb0c584d450ae615060b0a614337f7cc6cdfdc534f8fad713f7a8dcd09?apiKey=cf9de9a1ec724dc78b393139593f1cfb&" alt="" class="object-cover absolute inset-0 size-full" />
-            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/d696a05be17f05e5bf2085d3c177fdd1c4a0a4cef3495122a29549dac6a4dee0?apiKey=cf9de9a1ec724dc78b393139593f1cfb&" alt="Home icon" class="self-center aspect-[0.95] fill-white w-[19px]" />
-            <div class="relative mt-2">صفحه اصلی</div>
+        <div class="flex flex-col items-center pt-2">
+<!--          <div class="shrink-0 bg-yellow-300 rounded-2xl h-[3px] w-[45px]"></div>-->
+          <div class="cursor-pointer flex overflow-hidden relative flex-col px-2 py-2 aspect-[1.36] fill-[linear-gradient(180deg,)] w-[72px]">
+            <HomeIcon class="object-cover absolute inset-0 size-full" />
+            <div class="text-center mt-2">صفحه اصلی</div>
           </div>
         </div>
-        <div class="flex flex-col items-center">
-          <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/2410e9884e06812c971c78ee962f16286f7a270ac75661a0f2e28ea84bfecf7e?apiKey=cf9de9a1ec724dc78b393139593f1cfb&" alt="Favorites icon" class="self-center w-6 aspect-square" />
-          <div class="mt-1.5">علاقه مندی</div>
+        <div class="cursor-pointer flex flex-col items-center pt-2">
+          <FavouriteIcon class="object-cover absolute inset-0 size-full" />
+          <div class="text-center mt-2">علاقه مندی</div>
         </div>
-        <div class="flex flex-col items-center">
-          <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/81b01871d8a6c6724ba22f214d52de5ddac36d900d5730180603f7d960b22e99?apiKey=cf9de9a1ec724dc78b393139593f1cfb&" alt="Change panel icon" class="self-center w-6 aspect-square" />
-          <div class="mt-2">تغییر پنل</div>
+        <div class="cursor-pointer flex flex-col items-center pt-2">
+          <ChangePanelIcon class="object-cover absolute inset-0 size-full" />
+          <div class="text-center mt-2">تغییر پنل</div>
         </div>
-          <div class="flex flex-col items-center">
-            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/ae7ec5f6488a7028206b68ff96dc1e864b6fcbdc11f8475b7dbc9935106a29ee?apiKey=cf9de9a1ec724dc78b393139593f1cfb&" alt="Category icon" class="self-center w-6 aspect-square" />
-            <div class="mt-2.5">دسته بندی</div>
+          <div class="cursor-pointer flex flex-col items-center pt-2">
+            <CategoryIcon class="object-cover absolute inset-0 size-full" />
+            <div class="text-center mt-2">دسته بندی</div>
           </div>
-          <div class="flex flex-col items-center">
-            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/0253ef35d824e7cf5410726eb1deb26216856402016a945d11f8ed6841ab3af2?apiKey=cf9de9a1ec724dc78b393139593f1cfb&" alt="Profile icon" class="self-center aspect-[1.54] w-[37px]" />
-            <div class="mt-1.5">پروفایل</div>
+          <div class="cursor-pointer flex flex-col items-center pt-2">
+            <ProfileIcon class="object-cover absolute inset-0 size-full" />
+            <div class="text-center mt-2">پروفایل</div>
           </div>
       </div>
     </nav>
@@ -33,6 +32,11 @@
 
 <script setup lang="ts">
 
+import HomeIcon from "~/components/icons/BottomNavigation/HomeIcon.vue";
+import FavouriteIcon from "~/components/icons/BottomNavigation/FavouriteIcon.vue";
+import ChangePanelIcon from "~/components/icons/BottomNavigation/ChangePanelIcon.vue";
+import CategoryIcon from "~/components/icons/BottomNavigation/CategoryIcon.vue";
+import ProfileIcon from "~/components/icons/BottomNavigation/ProfileIcon.vue";
 </script>
 
 <style scoped>
