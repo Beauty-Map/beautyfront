fe7b339174a37c77ec3466e0f28f40216d38e66d
Saeed Heydari <saeedhpro@gmail.com>
2025-05-01 10:31:57 +0330

    fix user metas

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,121 +1,119 @@
-export default defineNuxtConfig({
-  devtools: { enabled: true },
+export default defineNuxtConfig(async () => {
 
-  app: {
-    head: {
-      htmlAttrs: { dir: 'rtl', lang: 'fa' },
-    },
-  },
-
-  css: ["@/assets/css/style.scss", "leaflet/dist/leaflet.css"],
-
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/device',
-    '@pinia/nuxt',
-    "nuxt-lodash",
-    // 'nuxt3-leaflet',
-    'dayjs-nuxt',
-    'nuxt-gtag',
-    '@nuxtjs/sitemap',
-  ],
-
-  sitemap: {
-    // hostname: 'http://localhost:3000',
-    hostname: 'https://beautymap.ir',
-    cacheTime: 0,
-    debug: true,
-    defaults: {
-      changefreq: 'daily',
-      priority: 1,
-      lastmod: new Date().toISOString()
-    },
-    strictNuxtContentPaths: true,
-    autoLastmod: true,
-    trailingSlash: true,
-    exclude: [
-    ],
-    urls: async () => {
-      const staticRoutes = [
-        '/',
-        '/aboutus',
-      ].map(route => ({
-        loc: route,
-        lastmod: new Date().toISOString(),
-        changefreq: 'daily',
-        priority: route === '/' ? 1.0 : 0.8
-      }));
-
-      const dynamicRoutes = await getDynamicRoutesFromAPI();
-      const formattedDynamicRoutes = dynamicRoutes.map(route => ({
-        loc: route,
-        lastmod: new Date().toISOString(),
-        changefreq: 'weekly',
-        priority: 0.7
-      }));
-
-      return [...staticRoutes, ...formattedDynamicRoutes];
+  async function getDynamicRoutesFromAPI() {
+    const routes = []
+    for (let i = 1; i <= 200; i++) {
+      routes.push(`/portfolios/${i}`)
+      routes.push(`/artists/${i}`)
     }
-  },
-
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-      routes: ['/sitemap.xml']
-    }
-  },
-
-  gtag: {
-    id: 'google-site-verification=rqwtUhnEx1ISv68hu7b69x_a-w6OGay5W0zd-bKgCb0'
-  },
-
-  tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.scss', {injectPosition: 'first'},],
-    configPath: 'tailwind.config',
-    exposeConfig: {
-      level: 2
-    },
-    config: {},
-    viewer: true,
-  },
-
-  vite: {
-    resolve: {
-      mainFields: [
-        'browser',
-        'module',
-        'main',
-        'jsnext:main',
-        'jsnext'
-      ]
-    }
-  },
-
-  runtimeConfig: {
-    public: {
-      // baseURL: 'http://localhost:8000/api',
-      // uploadURL: 'http://localhost:8000/api/upload',
-      baseURL: 'https://api.beautymap.ir/api',
-      uploadURL: 'https://api.beautymap.ir/api/upload',
-      walletAddress: 'UQCz1nOgL8ys5uKkza6hT1usfk8F97JIvdyD4UvucpnTAgDd',
-    }
-  },
-
-  devServer: {
-    host: '0.0.0.0',
-    port: 4000
-  },
-
-  compatibilityDate: '2025-04-20',
-})
-
-async function getDynamicRoutesFromAPI() {
-  let routes = [];
-  for (let i = 1; i <= 2000; i++) {
-    routes[i] = `/portfolios/${i}`
-    routes[i] = `/artists/${i}`
+    return routes
   }
-  return [];
-  // const { data } = await $fetch('http://localhost:8000/api/portfolios');
-  // return data.map(portfolio => `/portfolios/${portfolio.id}`);
-}
+
+  const dynamicRoutes = await getDynamicRoutesFromAPI()
+
+  return {
+    devtools: { enabled: true },
+    app: {
+      head: {
+        htmlAttrs: { dir: 'rtl', lang: 'fa' },
+      },
+    },
+
+    css: ["@/assets/css/style.scss", "leaflet/dist/leaflet.css"],
+
+    modules: [
+      '@nuxtjs/tailwindcss',
+      '@nuxtjs/device',
+      '@pinia/nuxt',
+      'nuxt-lodash',
+      'dayjs-nuxt',
+      'nuxt-gtag',
+      '@nuxtjs/sitemap',
+    ],
+
+    sitemap: {
+      hostname: 'https://beautymap.ir',
+      cacheTime: 0,
+      debug: true,
+      defaults: {
+        changefreq: 'daily',
+        priority: 1,
+        lastmod: new Date().toISOString()
+      },
+      strictNuxtContentPaths: true,
+      autoLastmod: true,
+      trailingSlash: true,
+      exclude: [],
+      urls: async () => {
+        const staticRoutes = [
+          '/',
+          '/aboutus',
+        ].map(route => ({
+          loc: route,
+          lastmod: new Date().toISOString(),
+          changefreq: 'daily',
+          priority: route === '/' ? 1.0 : 0.8
+        }))
+
+        const formattedDynamicRoutes = dynamicRoutes.map(route => ({
+          loc: route,
+          lastmod: new Date().toISOString(),
+          changefreq: 'weekly',
+          priority: 0.7
+        }))
+
+        return [...staticRoutes, ...formattedDynamicRoutes]
+      }
+    },
+
+    nitro: {
+      prerender: {
+        crawlLinks: true,
+        routes: [
+          '/sitemap.xml',
+        ]
+      }
+    },
+
+    gtag: {
+      id: 'google-site-verification=rqwtUhnEx1ISv68hu7b69x_a-w6OGay5W0zd-bKgCb0'
+    },
+
+    tailwindcss: {
+      cssPath: ['~/assets/css/tailwind.scss', { injectPosition: 'first' }],
+      configPath: 'tailwind.config',
+      exposeConfig: {
+        level: 2
+      },
+      config: {},
+      viewer: true,
+    },
+
+    vite: {
+      resolve: {
+        mainFields: [
+          'browser',
+          'module',
+          'main',
+          'jsnext:main',
+          'jsnext'
+        ]
+      }
+    },
+
+    runtimeConfig: {
+      public: {
+        baseURL: 'https://api.beautymap.ir/api',
+        uploadURL: 'https://api.beautymap.ir/api/upload',
+        walletAddress: 'UQCz1nOgL8ys5uKkza6hT1usfk8F97JIvdyD4UvucpnTAgDd'
+      }
+    },
+
+    devServer: {
+      host: '0.0.0.0',
+      port: 4000
+    },
+
+    compatibilityDate: '2025-04-20',
+  }
+})
